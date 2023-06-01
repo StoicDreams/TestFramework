25bfc3c60f67b11b1e08f8d943c9104b142db894
Erik Gassler <erikgassler@live.com>
2023-06-01 12:43:09 -0500

    Make IActions use builder pattern and support explicit exception expectations

--- StoicDreams.TestFramework/Collections/Actions.cs
+++ StoicDreams.TestFramework/Collections/Actions.cs
@@ -2,164 +2,213 @@
 
 public class Actions : IActions
 {
-	public Actions(object input)
-	{
-		Value = input;
-	}
+    public Actions(object input)
+    {
+        Value = input;
+    }
 
-	public void Act<T>(Func<T, object> action)
-	{
-		Result = action((T)Value);
-	}
+    public IActions Act<T>(Func<T, object> action)
+    {
+        Result = action((T)Value);
+        return this;
+    }
 
-	public void Act<T>(Func<T, Task<object>> action)
-	{
-		Result = action((T)Value).GetAwaiter().GetResult();
-	}
+    public IActions Act<T>(Func<T, Task<object>> action)
+    {
+        Result = action((T)Value).GetAwaiter().GetResult();
+        return this;
+    }
 
-	public void ActThrowsException(Action action)
-	{
-		try
-		{
-			action.Invoke();
-		}
-		catch (Exception ex)
-		{
-			Result = ex;
-			return;
-		}
-		throw new Exception("Exception was expected but no exception was thrown");
-	}
+    public IActions ActThrowsException(Action action)
+    {
+        try
+        {
+            action.Invoke();
+        }
+        catch (Exception ex)
+        {
+            Result = ex;
+            return this;
+        }
+        throw new Exception("Exception was expected but no exception was thrown");
+    }
 
-	public void ActThrowsException(Func<Task> action)
-	{
-		try
-		{
-			action.Invoke().GetAwaiter().GetResult();
-		}
-		catch (Exception ex)
-		{
-			Result = ex;
-			return;
-		}
-		throw new Exception("Exception was expected but no exception was thrown");
-	}
+    public IActions ActThrowsException(Func<Task> action)
+    {
+        try
+        {
+            action.Invoke().GetAwaiter().GetResult();
+        }
+        catch (Exception ex)
+        {
+            Result = ex;
+            return this;
+        }
+        throw new Exception("Exception was expected but no exception was thrown");
+    }
 
-	public void Assert<T>(Action<T?> action)
-	{
-		action((T?)Result);
-	}
+    public IActions ActThrowsException<TException>(Action action)
+        where TException : Exception
+    {
+        try
+        {
+            action.Invoke();
+        }
+        catch (TException ex)
+        {
+            Result = ex;
+            return this;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Exception of type {typeof(TException).FullName} was expected but {ex.GetType().FullName} was thrown instead.", ex);
+        }
+        throw new Exception("Exception was expected but no exception was thrown");
+    }
 
-	private object Value { get; }
-	private object? Result { get; set; }
+    public IActions ActThrowsException<TException>(Func<Task> action)
+        where TException : Exception
+    {
+        try
+        {
+            action.Invoke().GetAwaiter().GetResult();
+        }
+        catch (TException ex)
+        {
+            Result = ex;
+            return this;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Exception of type {typeof(TException).FullName} was expected but {ex.GetType().FullName} was thrown instead.", ex);
+        }
+        throw new Exception("Exception was expected but no exception was thrown");
+    }
+
+    public IActions Assert<T>(Action<T?> action)
+    {
+        action((T?)Result);
+        return this;
+    }
+
+    private object Value { get; }
+    private object? Result { get; set; }
 }
 
 public class Actions<TInstance> : IActions<TInstance>
-	where TInstance : class
+    where TInstance : class
 {
-	internal Actions(IServiceProvider serviceProvider, TInstance service)
-	{
-		ServiceProvider = serviceProvider;
-		Service = service;
-		Arrangement = new Arrangement<TInstance>(serviceProvider, service);
-	}
+    internal Actions(IServiceProvider serviceProvider, TInstance service)
+    {
+        ServiceProvider = serviceProvider;
+        Service = service;
+        Arrangement = new Arrangement<TInstance>(serviceProvider, service);
+    }
 
-	public IActions<TInstance> Act(Action<IArrangement<TInstance>> action)
-	{
-		action?.Invoke(Arrangement);
-		return this;
-	}
+    public IActions<TInstance> Act(Action<IArrangement<TInstance>> action)
+    {
+        action?.Invoke(Arrangement);
+        return this;
+    }
 
-	public IActions<TInstance> Act(Func<IArrangement<TInstance>, object?> action)
-	{
-		Arrangement.Result = action?.Invoke(Arrangement);
-		return this;
-	}
+    public IActions<TInstance> Act(Func<IArrangement<TInstance>, object?> action)
+    {
+        Arrangement.Result = action?.Invoke(Arrangement);
+        return this;
+    }
 
-	public IActions<TInstance> Act(Func<IArrangement<TInstance>, Task> action)
-	{
-		action?.Invoke(Arrangement).GetAwaiter().GetResult();
-		return this;
-	}
+    public IActions<TInstance> Act(Func<IArrangement<TInstance>, Task> action)
+    {
+        action?.Invoke(Arrangement).GetAwaiter().GetResult();
+        return this;
+    }
 
 
-	public IActions<TInstance> Act(Func<IArrangement<TInstance>, Task<object?>> action)
-	{
-		Arrangement.Result = action?.Invoke(Arrangement).GetAwaiter().GetResult();
-		return this;
-	}
+    public IActions<TInstance> Act(Func<IArrangement<TInstance>, Task<object?>> action)
+    {
+        Arrangement.Result = action?.Invoke(Arrangement).GetAwaiter().GetResult();
+        return this;
+    }
 
-	public IActions<TInstance> ActThrowsException(Action<IArrangement<TInstance>> action)
-	{
-		try
-		{
-			action.Invoke(Arrangement);
-		}
-		catch (Exception ex)
-		{
-			Arrangement.Result = ex;
-			return this;
-		}
-		throw new Exception("Exception was expected but no exception was thrown");
-	}
+    public IActions<TInstance> ActThrowsException(Action<IArrangement<TInstance>> action)
+    {
+        try
+        {
+            action.Invoke(Arrangement);
+        }
+        catch (Exception ex)
+        {
+            Arrangement.Result = ex;
+            return this;
+        }
+        throw new Exception("Exception was expected but no exception was thrown");
+    }
 
-	public IActions<TInstance> ActThrowsException(Func<IArrangement<TInstance>, Task> action)
-	{
-		try
-		{
-			action.Invoke(Arrangement).GetAwaiter().GetResult();
-		}
-		catch (Exception ex)
-		{
-			Arrangement.Result = ex;
-			return this;
-		}
-		throw new Exception("Exception was expected but no exception was thrown");
-	}
+    public IActions<TInstance> ActThrowsException(Func<IArrangement<TInstance>, Task> action)
+    {
+        try
+        {
+            action.Invoke(Arrangement).GetAwaiter().GetResult();
+        }
+        catch (Exception ex)
+        {
+            Arrangement.Result = ex;
+            return this;
+        }
+        throw new Exception("Exception was expected but no exception was thrown");
+    }
 
-	public IActions<TInstance> ActThrowsException<TException>(Action<IArrangement<TInstance>> action)
-		where TException : Exception
-	{
-		try
-		{
-			action.Invoke(Arrangement);
-		}
-		catch (TException ex)
-		{
-			Arrangement.Result = ex;
-			return this;
-		}
-		throw new Exception("Exception was expected but no exception was thrown");
-	}
+    public IActions<TInstance> ActThrowsException<TException>(Action<IArrangement<TInstance>> action)
+        where TException : Exception
+    {
+        try
+        {
+            action.Invoke(Arrangement);
+        }
+        catch (TException ex)
+        {
+            Arrangement.Result = ex;
+            return this;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Exception of type {typeof(TException).FullName} was expected but {ex.GetType().FullName} was thrown instead.", ex);
+        }
+        throw new Exception("Exception was expected but no exception was thrown");
+    }
 
-	public IActions<TInstance> ActThrowsException<TException>(Func<IArrangement<TInstance>, Task> action)
-		where TException : Exception
-	{
-		try
-		{
-			action.Invoke(Arrangement).GetAwaiter().GetResult();
-		}
-		catch (TException ex)
-		{
-			Arrangement.Result = ex;
-			return this;
-		}
-		throw new Exception("Exception was expected but no exception was thrown");
-	}
+    public IActions<TInstance> ActThrowsException<TException>(Func<IArrangement<TInstance>, Task> action)
+        where TException : Exception
+    {
+        try
+        {
+            action.Invoke(Arrangement).GetAwaiter().GetResult();
+        }
+        catch (TException ex)
+        {
+            Arrangement.Result = ex;
+            return this;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Exception of type {typeof(TException).FullName} was expected but {ex.GetType().FullName} was thrown instead.", ex);
+        }
+        throw new Exception("Exception was expected but no exception was thrown");
+    }
 
-	public IActions<TInstance> Assert(Action<IArrangement<TInstance>> action)
-	{
-		action?.Invoke(Arrangement);
-		return this;
-	}
+    public IActions<TInstance> Assert(Action<IArrangement<TInstance>> action)
+    {
+        action?.Invoke(Arrangement);
+        return this;
+    }
 
-	public IActions<TInstance> Assert(Func<IArrangement<TInstance>, Task> action)
-	{
-		action?.Invoke(Arrangement).GetAwaiter().GetResult();
-		return this;
-	}
+    public IActions<TInstance> Assert(Func<IArrangement<TInstance>, Task> action)
+    {
+        action?.Invoke(Arrangement).GetAwaiter().GetResult();
+        return this;
+    }
 
-	private Arrangement<TInstance> Arrangement { get; }
-	private TInstance Service { get; }
-	private IServiceProvider ServiceProvider { get; }
+    private Arrangement<TInstance> Arrangement { get; }
+    private TInstance Service { get; }
+    private IServiceProvider ServiceProvider { get; }
 }
--- StoicDreams.TestFramework/Interfaces/IActions.cs
+++ StoicDreams.TestFramework/Interfaces/IActions.cs
@@ -5,38 +5,52 @@
 /// </summary>
 public interface IActions
 {
-	/// <summary>
-	/// Act on test, returning result of action.
-	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	/// <param name="action"></param>
-	void Act<T>(Func<T, object> action);
+    /// <summary>
+    /// Act on test, returning result of action.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="action"></param>
+    IActions Act<T>(Func<T, object> action);
 
-	/// <summary>
-	/// Act on test, returning result of action from async method.
-	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	/// <param name="action"></param>
-	void Act<T>(Func<T, Task<object>> action);
+    /// <summary>
+    /// Act on test, returning result of action from async method.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="action"></param>
+    IActions Act<T>(Func<T, Task<object>> action);
 
-	/// <summary>
-	/// Act on test, expecting an error to be thrown.
-	/// </summary>
-	/// <param name="action"></param>
-	void ActThrowsException(Action action);
+    /// <summary>
+    /// Act on test, expecting an error to be thrown.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions ActThrowsException(Action action);
 
-	/// <summary>
-	/// Act on test, expecting an error to be thrown from async method.
-	/// </summary>
-	/// <param name="action"></param>
-	void ActThrowsException(Func<Task> action);
+    /// <summary>
+    /// Act on test, expecting a specific error to be thrown.
+    /// </summary>
+    /// <typeparam name="TException"></typeparam>
+    /// <param name="action"></param>
+    IActions ActThrowsException<TException>(Action action) where TException : Exception;
 
-	/// <summary>
-	/// Assert results of test, being passed result from last Act result.
-	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	/// <param name="action"></param>
-	void Assert<T>(Action<T?> action);
+    /// <summary>
+    /// Act on test, expecting an error to be thrown from async method.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions ActThrowsException(Func<Task> action);
+
+    /// <summary>
+    /// Act on test, expecting a specific error to be thrown from async method.
+    /// </summary>
+    /// <typeparam name="TException"></typeparam>
+    /// <param name="action"></param>
+    IActions ActThrowsException<TException>(Func<Task> action) where TException : Exception;
+
+    /// <summary>
+    /// Assert results of test, being passed result from last Act result.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="action"></param>
+    IActions Assert<T>(Action<T?> action);
 }
 
 /// <summary>
@@ -45,63 +59,63 @@ public interface IActions
 /// <typeparam name="TInstance"></typeparam>
 public interface IActions<TInstance> where TInstance : class
 {
-	/// <summary>
-	/// Act on test with no result.
-	/// </summary>
-	/// <param name="action"></param>
-	IActions<TInstance> Act(Action<IArrangement<TInstance>> action);
+    /// <summary>
+    /// Act on test with no result.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions<TInstance> Act(Action<IArrangement<TInstance>> action);
 
-	/// <summary>
-	/// Act on test and return a result.
-	/// </summary>
-	/// <param name="action"></param>
-	IActions<TInstance> Act(Func<IArrangement<TInstance>, object?> action);
+    /// <summary>
+    /// Act on test and return a result.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions<TInstance> Act(Func<IArrangement<TInstance>, object?> action);
 
-	/// <summary>
-	/// Act on test.
-	/// </summary>
-	/// <param name="action"></param>
-	IActions<TInstance> Act(Func<IArrangement<TInstance>, Task> action);
+    /// <summary>
+    /// Act on test.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions<TInstance> Act(Func<IArrangement<TInstance>, Task> action);
 
-	/// <summary>
-	/// Act on test, returning result from async method.
-	/// </summary>
-	/// <param name="action"></param>
-	IActions<TInstance> Act(Func<IArrangement<TInstance>, Task<object?>> action);
+    /// <summary>
+    /// Act on test, returning result from async method.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions<TInstance> Act(Func<IArrangement<TInstance>, Task<object?>> action);
 
-	/// <summary>
-	/// Act on test, expecting an error to be thrown calling method.
-	/// </summary>
-	/// <param name="action"></param>
-	IActions<TInstance> ActThrowsException(Action<IArrangement<TInstance>> action);
+    /// <summary>
+    /// Act on test, expecting an error to be thrown calling method.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions<TInstance> ActThrowsException(Action<IArrangement<TInstance>> action);
 
-	/// <summary>
-	/// Act on test, expecting an error to be thrown calling async method.
-	/// </summary>
-	/// <param name="action"></param>
-	IActions<TInstance> ActThrowsException(Func<IArrangement<TInstance>, Task> action);
+    /// <summary>
+    /// Act on test, expecting an error to be thrown calling async method.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions<TInstance> ActThrowsException(Func<IArrangement<TInstance>, Task> action);
 
-	/// <summary>
-	/// Act on test, expecting an error to be thrown calling method.
-	/// </summary>
-	/// <param name="action"></param>
-	IActions<TInstance> ActThrowsException<TException>(Action<IArrangement<TInstance>> action) where TException : Exception;
+    /// <summary>
+    /// Act on test, expecting an error to be thrown calling method.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions<TInstance> ActThrowsException<TException>(Action<IArrangement<TInstance>> action) where TException : Exception;
 
-	/// <summary>
-	/// Act on test, expecting an error to be thrown calling async method.
-	/// </summary>
-	/// <param name="action"></param>
-	IActions<TInstance> ActThrowsException<TException>(Func<IArrangement<TInstance>, Task> action) where TException : Exception;
+    /// <summary>
+    /// Act on test, expecting an error to be thrown calling async method.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions<TInstance> ActThrowsException<TException>(Func<IArrangement<TInstance>, Task> action) where TException : Exception;
 
-	/// <summary>
-	/// Run assertions on results of test.
-	/// </summary>
-	/// <param name="action"></param>
-	IActions<TInstance> Assert(Action<IArrangement<TInstance>> action);
+    /// <summary>
+    /// Run assertions on results of test.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions<TInstance> Assert(Action<IArrangement<TInstance>> action);
 
-	/// <summary>
-	/// Run assertions on results of test with ability to call async methods.
-	/// </summary>
-	/// <param name="action"></param>
-	IActions<TInstance> Assert(Func<IArrangement<TInstance>, Task> action);
+    /// <summary>
+    /// Run assertions on results of test with ability to call async methods.
+    /// </summary>
+    /// <param name="action"></param>
+    IActions<TInstance> Assert(Func<IArrangement<TInstance>, Task> action);
 }
